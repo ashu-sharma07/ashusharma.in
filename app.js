36810b29f075496e71c351ff9fc78c537d12b2fa
Ashu Sharma <ashusharma0771@gmail.com>
2022-07-16 22:48:49 +0530

    handle 404

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res)=>{
     res.sendFile(__dirname+"/index.html");
 });
 
+app.get("/:anything", function(req, res){
+    res.send("<h1>404 Page Don't Exist </h1><a href='/\'>Get Back to Home</a>")
+});
+
 const domain = "127.0.0.1"
 const port = 3000;
 app.listen(port,domain,()=>{
