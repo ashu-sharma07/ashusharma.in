cb97510787867fb0c38d0ef217dbc50ca81acb90
Ashu Sharma <ashusharma0771@gmail.com>
2022-07-16 22:58:51 +0530

    domain update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/:anything", function(req, res){
     res.send("<h1>404 Page Don't Exist </h1><a href='/\'>Get Back to Home</a>")
 });
 
-const domain = "127.0.0.1"
+const domain = "localhost"
 const port = 3000;
 app.listen(port,domain,()=>{
     console.log(`Application running on ${domain}:${port}`)
